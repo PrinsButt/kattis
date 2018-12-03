69adaf106dadfb9290c37e7c9453a72de885d857
Prins <prinsbutt@gmail.com>
2018-12-03 17:41:31 +0000

    Create Main.java
    Added code to solve problem id: timeloop

---
+++ 3-timeloop/Main.java
@@ -0,0 +1,15 @@
+// Problem url: https://open.kattis.com/problems/timeloop
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int magicNumber = reader.nextInt();
+    reader.close();
+
+    for (int i=1; i <= magicNumber; i++) {
+      System.out.println("" + i + " Abracadabra");
+    }
+  }
+}
