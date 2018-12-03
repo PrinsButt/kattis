69c3138824cc2483a6dbc7eea864042f616d48fb
Prins Butt <prinsbutt@gmail.com>
2018-12-03 20:10:51 +0000

    Solved problem with id autori.

---
+++ 13-atouri/Main.java
@@ -0,0 +1,21 @@
+// Problem url: https://open.kattis.com/problems/autori
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    String longName = reader.nextLine();
+    reader.close();
+
+    String shortName = "";
+
+    for (char letter : longName.toCharArray()) {
+      if (letter < 'a' && letter != '-') {
+        shortName += letter;
+      }
+    }
+
+    System.out.println(shortName);
+  }
+}
