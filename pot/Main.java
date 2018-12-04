b3a7a96658a3b4aedf7be300124dc699d02ea5e4
Prins Butt <prinsbutt@gmail.com>
2018-12-04 06:39:12 +0000

    Solved problem with id: pot.

---
+++ pot/Main.java
@@ -0,0 +1,23 @@
+// Problem url: https://open.kattis.com/problems/pot
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int numEntries = Integer.parseInt(reader.nextLine());
+    
+    int sum = 0;
+
+    for (int index=0; index < numEntries; index++) {
+      String entry = reader.nextLine();
+      int base = Integer.parseInt(entry.substring(0, entry.length() - 1));
+      int exponent = Integer.parseInt(entry.substring(entry.length() - 1)); 
+      sum += Math.pow(base, exponent);
+    }
+
+    reader.close();
+
+    System.out.println(sum);
+  }
+}
