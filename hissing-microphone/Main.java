c5c24c16e108963b298a4650057a26b359795b9e
Prins Butt <prinsbutt@gmail.com>
2018-12-07 22:42:35 +0000

    Solved problem with id: hissingmicrophone.

---
+++ hissing-microphone/Main.java
@@ -0,0 +1,16 @@
+// Problem url: https://open.kattis.com/problems/hissingmicrophone
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    String sentence = reader.nextLine();
+    reader.close();
+
+    String affected = sentence.replaceAll("s{2,}", "");
+
+    if (affected.length() != sentence.length()) System.out.println("hiss");
+    else System.out.println("no hiss"); 
+  }
+}
