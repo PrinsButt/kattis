b1a19a681466ee1f5eee2c7b6c27968e3322b769
Prins Butt <prinsbutt@gmail.com>
2018-12-07 21:34:37 +0000

    Solved problem with ids pet and oddities.

---
+++ oddities/Main.java
@@ -0,0 +1,25 @@
+// Problem url: https://open.kattis.com/problems/oddities
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int numTestCases = Integer.parseInt(reader.nextLine());
+
+    String output = "";
+
+    for (int testCase=0; testCase < numTestCases; testCase++) {
+      int numX = Integer.parseInt(reader.nextLine());
+	  
+      if (numX % 2 == 0) output += (numX + " is even");
+      else output += (numX + " is odd");
+
+      if (testCase != numTestCases - 1) output += "\n";
+    }
+
+    reader.close();
+
+    System.out.println(output);
+  }
+}
---
+++ pet/Main.java
@@ -0,0 +1,30 @@
+// Problem url: https://open.kattis.com/problems/pet
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+
+    int highestPoints = 0;
+    int winner = 0;
+
+    for (int grader=1; grader <= 5; grader++) {
+      int sum = 0;
+
+      for (int index=0; index < 4; index++) {
+        sum += reader.nextInt();
+      }
+
+      if (sum > highestPoints) {
+        highestPoints = sum;
+        winner = grader;
+      }
+
+      reader.nextLine();
+    }
+
+    reader.close();
+    System.out.println(winner + " " + highestPoints);
+  }
+}
