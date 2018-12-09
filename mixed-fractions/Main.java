eb8a1fa1eaab4424289d74a46b973efd9f640a2e
Prins Butt <prinsbutt@gmail.com>
2018-12-09 23:33:57 +0000

    Solved problem with id: mixedfractions.

---
+++ mixed-fractions/Main.java
@@ -0,0 +1,39 @@
+// Problem url: https://open.kattis.com/problems/mixedfractions
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+class Main {
+
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    
+    StringBuilder output = new StringBuilder();
+
+    while (true) {
+      try {
+        BigInteger numerator = reader.nextBigInteger();
+        BigInteger denominator = reader.nextBigInteger();
+        reader.nextLine();
+
+        if (numerator.compareTo(new BigInteger("0")) == 0 
+            && denominator.compareTo(new BigInteger("0")) == 0) break;
+
+        if (denominator.compareTo(new BigInteger("0")) == 0) break;
+
+    
+        output.append(numerator.divide(denominator));
+        output.append(" ");
+        output.append(numerator.mod(denominator));
+        output.append(" / ");
+        output.append(denominator);
+        output.append("\n");
+      }
+      catch(Exception e) { break; }
+    }
+
+    reader.close();
+
+    System.out.print(output);
+  }
+}
