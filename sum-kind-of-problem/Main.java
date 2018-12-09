21993ee50b412bc036e38cb5c8889fd6e27205b4
Prins Butt <prinsbutt@gmail.com>
2018-12-09 17:17:19 +0000

    Solved problem with id: sumkindofproblem.

---
+++ sum-kind-of-problem/Main.java
@@ -0,0 +1,35 @@
+// Problem url: https://open.kattis.com/problems/sumkindofproblem
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int numDataSets = Integer.parseInt(reader.nextLine());
+
+    StringBuilder output = new StringBuilder();
+
+    for (int dataSet=0; dataSet < numDataSets; dataSet++) {
+      int id = reader.nextInt();
+      int numN = reader.nextInt();
+      reader.nextLine();
+
+      int sumNaturals = (numN * (numN + 1)) / 2;
+      int sumEvens = numN * (numN + 1);
+      int sumOdds = (int) Math.pow(numN, 2);
+
+      output.append(id);
+      output.append(" ");
+      output.append(sumNaturals);
+      output.append(" ");
+      output.append(sumOdds);
+      output.append(" ");
+      output.append(sumEvens);
+      output.append("\n");
+    }
+
+    reader.close();
+
+    System.out.println(output.toString());
+  }
+}
