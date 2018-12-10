1b6dce820cea7704ebcfa18a76e56849804399ed
Prins Butt <prinsbutt@gmail.com>
2018-12-10 01:17:11 +0000

    Solved problem with id: symmetricorder.

---
+++ symmetric-order/Main.java
@@ -0,0 +1,47 @@
+// Problem url: https://open.kattis.com/problems/symmetricorder
+
+import java.util.Scanner;
+
+class Main {
+
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+
+    StringBuilder output = new StringBuilder();
+    int setId = 0;
+
+    while (true) {
+      int numNames = Integer.parseInt(reader.nextLine());
+
+      if (numNames == 0) break;
+
+      StringBuilder firstHalf = new StringBuilder();
+      StringBuilder secondHalf = new StringBuilder();
+
+      setId++;
+
+      for (int index=0; index < numNames; index++) {
+        String name = reader.nextLine();
+
+        if (index % 2 == 0) {
+          firstHalf.append(name);
+          firstHalf.append("\n");
+        }
+        else {
+          secondHalf.insert(0, "\n"); 
+          secondHalf.insert(0, name);
+        }
+      }
+
+      output.append("SET ");
+      output.append(setId);
+      output.append("\n");
+      output.append(firstHalf.toString());
+      output.append(secondHalf.toString());
+    }
+
+    reader.close();
+
+    System.out.print(output);
+  }
+}
