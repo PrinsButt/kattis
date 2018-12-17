990d4d037e99b86b74cea9b4063209d751cca7de
Prins Butt <prinsbutt@gmail.com>
2018-12-17 13:19:10 +0000

    Solved problems with id: aah, skocimis and sumsquareddigits.

---
+++ aaah/Main.java
@@ -0,0 +1,16 @@
+// Problem url: https://open.kattis.com/problems/aaah
+
+import java.util.Scanner;
+
+class Main {
+
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    String marius = reader.nextLine();
+    String doctor = reader.nextLine();
+    reader.close();
+
+    if (marius.length() >= doctor.length()) System.out.println("go");
+    else System.out.println("no");
+  }
+}
---
+++ skocimis/Main.java
@@ -0,0 +1,29 @@
+// Problem url: https://open.kattis.com/problems/skocimis
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int kangarooA = reader.nextInt();
+    int kangarooB = reader.nextInt();
+    int kangarooC = reader.nextInt();
+    reader.close();
+
+    int moves = 0;
+    int left = kangarooB - kangarooA;
+    int right = kangarooC - kangarooB;
+
+    if (left > right) {
+      if (left > 1) moves = left - 1;
+    }
+    else if (right > left) {
+      if (right > 1) moves = right - 1;
+    }
+    else if (left == right) {
+      if (left > 1) moves = left - 1;
+    }
+
+    System.out.println(moves);
+  }
+}
