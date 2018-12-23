8e05a222be4c23dd7446e26127e94342a123e821
Prins Butt <prinsbutt@gmail.com>
2018-12-23 10:21:55 +0000

    Solved problem with id: humancannonball2.

---
+++ human-cannon-ball-2/Main.java
@@ -0,0 +1,31 @@
+// Problem url: https://open.kattis.com/problems/humancannonball2
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int numTestCases = Integer.parseInt(reader.nextLine());
+
+    StringBuilder output = new StringBuilder();
+
+    for (int testCase=0; testCase < numTestCases; testCase++) {
+      double v0 = reader.nextDouble();
+      double theta = reader.nextDouble();
+      double x1 = reader.nextDouble();
+      double h1 = reader.nextDouble();
+      double h2 = reader.nextDouble();
+      reader.nextLine();
+
+      double t = x1 / (v0 * Math.cos(Math.toRadians(theta)));
+      double y = (v0 * t * Math.sin(Math.toRadians(theta))) - (0.5 * 9.81 * Math.pow(t, 2));
+
+      if (y >= h1 + 1 && y <= h2 - 1) output.append("Safe\n");
+      else output.append("Not Safe\n");
+    }
+
+    reader.close();
+
+    System.out.print(output);
+  }
+}
