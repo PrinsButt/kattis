339a029cecb92f6bea4f467cae480f481d378b66
Prins Butt <prinsbutt@gmail.com>
2019-01-01 16:25:47 +0000

    Optimised solution for problem with id: relocation.  Solution is better but needs further improving to meet necessary time requirements.

--- relocation/Main.java
+++ relocation/Main.java
@@ -1,5 +1,7 @@
 // Problem url: https://open.kattis.com/problems/relocation
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 class Main {
@@ -9,19 +11,27 @@ class Main {
     int requests = reader.nextInt();
     reader.nextLine();
 
+    Map<Integer, Integer> locationsMap = new HashMap<Integer, Integer>();
+
     // initial locations
-    String locations[] = reader.nextLine().split(" ");
+    for (int company=1; company <= companies; company++) {
+      locationsMap.put(company, reader.nextInt());
+    }
+
+    reader.nextLine();
 
     // process requests
     StringBuilder output = new StringBuilder();
 
-    for (int request=0; request < requests; request++) {
+    for (int request=1; request <= requests; request++) {
       String parts[] = reader.nextLine().split(" ");
 
-      if (parts[0].equals("1")) locations[Integer.parseInt(parts[1]) - 1] = parts[2];
+      if (parts[0].equals("1")) {
+        locationsMap.replace(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
+      }
       else {
-        int firstCompany = Integer.parseInt(locations[Integer.parseInt(parts[1]) - 1]);
-        int secondCompany = Integer.parseInt(locations[Integer.parseInt(parts[2]) - 1]);
+        int firstCompany = locationsMap.get(Integer.parseInt(parts[1]));
+        int secondCompany = locationsMap.get(Integer.parseInt(parts[2]));
 
         output.append(Math.abs(firstCompany - secondCompany)).append("\n");
       }
