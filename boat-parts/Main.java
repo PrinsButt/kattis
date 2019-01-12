aaff4e11bf23c24e172c7525d7af0131512e50f4
Prins Butt <prinsbutt@gmail.com>
2019-01-12 19:37:26 +0000

    Solved problem with id: boatparts.

---
+++ boat-parts/Main.java
@@ -0,0 +1,26 @@
+// Problem url: https://open.kattis.com/problems/boatparts
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int numParts = reader.nextInt();
+    int numDays = reader.nextInt();
+    reader.nextLine();
+
+    Set<String> partSet = new HashSet<String>();
+
+    int answer = 0;
+
+    for (int day=1; day <= numDays; day++) {
+      partSet.add(reader.nextLine());
+      if (answer == 0 && partSet.size() == numParts) answer = day;
+    }
+
+    if (answer == 0) System.out.println("paradox avoided");
+    else System.out.println(answer);
+  }
+}
