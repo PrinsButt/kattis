c0b26b98810f2939e6c3a6ec574b10a894d66f85
Prins Butt <prinsbutt@gmail.com>
2019-01-28 19:55:06 +0000

    Solved problem with id: helpaphd.

---
+++ help-a-phd/Main.java
@@ -0,0 +1,26 @@
+// Problem url: https://open.kattis.com/problems/helpaphd
+
+import java.util.Scanner;
+
+class Main {
+
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int testCases = Integer.parseInt(reader.nextLine());
+    
+    StringBuilder output = new StringBuilder();
+
+    for (int testCase=0; testCase < testCases; testCase++) {
+      String expression = reader.nextLine();
+      if (expression.equals("P=NP")) output.append("skipped\n");
+      else {
+        String parts[] = expression.split("\\+");
+        output.append(Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]));
+        output.append("\n");
+      }
+    }
+
+    reader.close();
+    System.out.print(output);
+  }
+}
