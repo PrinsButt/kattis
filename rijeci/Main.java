6b9ee2cc12da797a28ae1528cc469e8617e8eff2
Prins Butt <prinsbutt@gmail.com>
2019-04-18 12:08:05 +0100

    Improved solution for problem with id: rijeci using fibonacii series.

--- rijeci/Main.java
+++ rijeci/Main.java
@@ -1,31 +1,21 @@
 // Problem url: https://open.kattis.com/problems/rijeci
 
-import java.util.Iterator;
 import java.util.Scanner;
 
 class Main {
 
+  public static int fibonacii(int n) {
+    if (n <= 1) return n;
+    else return fibonacii(n - 1) + fibonacii(n - 2);
+  }
+
   public static void main(String[] args) {
-    
     Scanner reader = new Scanner(System.in);
     int numPresses = Integer.parseInt(reader.nextLine());
-    String letters = "A";
-
-    for (int press=0; press < numPresses; press++) {
-      StringBuffer modified = new StringBuffer();
-
-      for (char letter : letters.toCharArray()) {
-          if (letter == 'A') modified.append("B");
-          else modified.append("BA");
-      }
-
-      letters = modified.toString();
-    }
-
-    int totalA = letters.replaceAll("B", "").length();
-    int totalB = letters.length() - totalA;
-
     reader.close();
-    System.out.print(totalA + " " + totalB);
+
+    System.out.print(fibonacii(numPresses - 1));
+    System.out.print(" ");
+    System.out.println(fibonacii(numPresses));
   }
 }
