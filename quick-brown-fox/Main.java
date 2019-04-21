e0c75d392d29f4ae137aa255ef1397ecfa541a64
Prins Butt <prinsbutt@gmail.com>
2019-04-21 02:28:55 +0100

    Solved problem with id: quickbrownfox.

---
+++ quick-brown-fox/Main.java
@@ -0,0 +1,34 @@
+// Problem url: https://open.kattis.com/problems/quickbrownfox
+
+import java.util.Scanner;
+
+class Main {
+
+  private static final String ATOZ = "abcdefghijklmnopqrstuvwxyz";
+
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int numLines = Integer.parseInt(reader.nextLine());
+    StringBuilder output = new StringBuilder();
+
+    for (int index=0; index < numLines; index++) {
+      String line = reader.nextLine().toLowerCase();
+      StringBuilder missing = new StringBuilder();
+
+      for (char c : ATOZ.toCharArray()) {
+        if (line.indexOf(c) < 0) {
+          missing.append(c);
+        }
+      }
+
+      if (missing.toString().isEmpty()) {
+        output.append("pangram\n");
+      } else {
+        output.append("missing ").append(missing).append("\n");
+      }
+    }
+
+    reader.close();
+    System.out.print(output);
+  }
+}
